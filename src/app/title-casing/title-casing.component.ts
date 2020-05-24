58e2e69757f452ea2cd2ccf96953dbeed52abbc8
Curt Grogan <curtgrogan@outlook.com>
2020-05-24 17:10:45 -0400

    Style binding added.

--- src/app/title-casing/title-casing.component.ts
+++ src/app/title-casing/title-casing.component.ts
@@ -8,9 +8,15 @@ import { Component, OnInit } from "@angular/core";
 export class TitleCasingComponent implements OnInit {
   userInput;
   minorWords;
+  titleCaseExplained;
   constructor() {
     this.userInput = "";
     this.minorWords = ["a", "an", "the", "of"];
+    this.titleCaseExplained = `A string is considered to be in title case if each word in the string is
+    either capitalized with only the first letter in uppercase. Except in the
+    case of a minor word, which will be all lower case unless it is the first
+    word of the string, in which case it will be capitalized like a 'normal'
+    word."`;
   }
 
   ngOnInit(): void {}
