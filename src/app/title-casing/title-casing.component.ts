2b46fd44cc9b6dfcfa2233405dad081341f367be
Curt Grogan <curtgrogan@outlook.com>
2020-05-25 08:01:16 -0400

    More style.

--- src/app/title-casing/title-casing.component.ts
+++ src/app/title-casing/title-casing.component.ts
@@ -10,13 +10,13 @@ export class TitleCasingComponent implements OnInit {
   minorWords;
   titleCaseExplained;
   constructor() {
-    this.userInput = "";
-    this.minorWords = ["a", "an", "the", "of"];
-    this.titleCaseExplained = `A string is considered to be in title case if each word in the string is
-    either capitalized with only the first letter in uppercase. Except in the
+    this.userInput = "a user sHoUld deLete THIS input line of text";
+    this.minorWords = ["a", "an", "the", "of", "is"];
+    this.titleCaseExplained = `a string is considered to be in title case if each word in the string is
+    either capitalized with only the first letter in uppercase. except in the
     case of a minor word, which will be all lower case unless it is the first
-    word of the string, in which case it will be capitalized like a 'normal'
-    word."`;
+    word of the string, in which case it will be capitalized like a major
+    word.`;
   }
 
   ngOnInit(): void {}
